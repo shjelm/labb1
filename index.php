224e36377af91dabcced58648e0a26a88a9399c8
unknown <sh222mw@.lnu.se>
2013-09-18 14:29:25 +0200

    Slutgiltiga

--- index.php
+++ index.php
@@ -1,48 +1,43 @@
-<?php 
+<?php
 
 session_start();
 
 $message = null;
 $successMessage = null;
 
-if(isset($_POST['logout'])){
+if (isset($_POST['logout'])) {
 	getLogOutPage();
 }
 
-if($_POST){
-	if(isset($_POST)){	
-		if($_POST['UserName'] == "Admin" && $_POST['Password'] == "Password"){
+if ($_POST) {
+	if (isset($_POST)) {
+		if ($_POST['UserName'] == "Admin" && $_POST['Password'] == "Password") {
 			$_SESSION['mySession'] = true;
-			
+
 			$successMessage = '<p>Inloggningen lyckades</p>';
-		}
-		else if(empty($_POST['UserName'])){
-			$message='<p>Användarnamn saknas</p>';
-				}
-		else if(empty($_POST['Password'])){
-			$message='<p>Lösenord saknas</p>';
-		}
-		else{ $message='<p>Felaktigt användarnamn och/eller lösenord</p>';
+		} else if (empty($_POST['UserName'])) {
+			$message = '<p>Användarnamn saknas</p>';
+		} else if (empty($_POST['Password'])) {
+			$message = '<p>Lösenord saknas</p>';
+		} else { $message = '<p>Felaktigt användarnamn och/eller lösenord</p>';
 		}
 	}
 }
-			
-if(isset($_SESSION['mySession'])){
+
+if (isset($_SESSION['mySession'])) {
 	getLoggedInPage($successMessage);
-}
-else{
+} else {
 	getPage($message);
 }
 
-
-function getPage($message){
+function getPage($message) {
 	$value = null;
-	
-	if(isset($_POST['UserName'])){
+
+	if (isset($_POST['UserName'])) {
 		$value = $_POST['UserName'];
 	}
-	
-	$html  ='<!DOCTYPE HTML>
+
+	$html = '<!DOCTYPE HTML>
 			 <html>
 					<head>
 						<title> Laboration 1 sh222mw </title>
@@ -54,25 +49,25 @@ function getPage($message){
 						<h2>Ej inloggad</h2>
 						<fieldset>
 							<legend>Skriv in användarnamn och lösenord</legend>
-								<form method="post" action="index.php">
+								<form method="post" action="?login">
 									<label for="UserName">Användarnamn: </label>
-									<input type="text" name="UserName" id="UserName" value="'.$value.'">
+									<input type="text" name="UserName" id="UserName" value="' . $value . '">
 									<label for="Password">Lösenord: </label>
 									<input type="password" name="Password" id="Password" value="">
 							      	<input type="submit" name="login" value="Logga in" />
 						    	</form>';
-								
-	$html.= $message;
-						
-	$html.= '</fieldset>
-			    <p class="time">'.getClock().'</p>	
+
+	$html .= $message;
+
+	$html .= '</fieldset>
+			    <p class="time">' . getClock() . '</p>	
 				</body>
 			</html>';
-			
+
 	echo $html;
 }
 
-function getLoggedInPage($successMessage){
+function getLoggedInPage($successMessage) {
 	$loggedInHTML = '<!DOCTYPE HTML>
 					 <html>
 						<head>
@@ -82,27 +77,27 @@ function getLoggedInPage($successMessage){
 							<h1>Laboration 1 sh222mw</h1>
 						</head>
 						<body>
-							<h2> Admin är inloggad </h2>'.$successMessage.' 
-							<form method="post" action="">
+							<h2> Admin är inloggad </h2>' . $successMessage . ' 
+							<form method="post" action="?logout">
 							<input type="submit" name="logout" value="Logga ut" /> 
 							</form>
-							<p class="time">'.getClock().'</p>	
+							<p class="time">' . getClock() . '</p>	
 						</body>
 					</html>';
-				
-				echo $loggedInHTML;
+
+	echo $loggedInHTML;
 }
 
-function getLogOutPage(){
+function getLogOutPage() {
 	unset($_SESSION['mySession']);
-	
+
 	$message = "<p>Du har loggat ut</p>";
-	
+
 	getPage($message);
-	exit;
+	exit ;
 }
 
-function getClock(){
+function getClock() {
 	setlocale(LC_ALL, "swedish");
 	return strftime('%A, den %d %B år %Y. Klockan är: [%H:%M:%S] ');
 }
